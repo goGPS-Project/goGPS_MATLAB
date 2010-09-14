be45f73fc7306f5516c60ff3f63727a6c24320ee
ege010 <ege010@08326c51-6061-44f8-bb4b-f620d3b48b87>
2010-09-14 16:01:20 +0000

    Changed model error standard deviation values from X,Y,Z to E,N,U

--- trunk/kalman_goGPS_cod_init.m
+++ trunk/kalman_goGPS_cod_init.m
@@ -44,7 +44,7 @@ function kalman_goGPS_cod_init (pos_M, time, Eph, iono, pr1_Rsat, pr1_Msat, ...
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %----------------------------------------------------------------------------------------------
 
-global sigmaq0 sigmaq_velx sigmaq_vely sigmaq_velz
+global sigmaq0
 global cutoff o1 o2 o3
 
 global Xhat_t_t X_t1_t T I Cee conf_sat conf_cs pivot pivot_old
@@ -77,12 +77,6 @@ T = [T0      Z_o1_o1 Z_o1_o1;
 
 %identity matrix for following computations
 I = eye(o3);
-
-%model error covariance matrix
-Cvv = zeros(o3);
-Cvv(o1,o1) = sigmaq_velx;
-Cvv(o2,o2) = sigmaq_vely;
-Cvv(o3,o3) = sigmaq_velz; %#ok<NASGU>
 
 %--------------------------------------------------------------------------------------------
 % SATELLITE SELECTION
