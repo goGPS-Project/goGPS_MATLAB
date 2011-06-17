33db5ddaa532a19277b885a5c5a992103415ec0d
ege010 <eugenio.realini@gmail.com>
2011-06-17 04:15:53 +0000

    Julian date - GPS week/seconds of week conversion using a different algorithm

--- utility/gps/gps_time.m
+++ utility/gps/gps_time.m
@@ -17,20 +17,10 @@ function [week, sec_of_week] = gps_time(julday)
 %----------------------------------------------------------------------------------------------
 %                           goGPS v0.2.0 beta
 %
-% Copyright (C) Kai Borre
-% Kai Borre 05-20-96
-%
-% Adapted by Mirko Reguzzoni, Eugenio Realini, 2009
+% Copyright (C) 2009-2011 Mirko Reguzzoni, Eugenio Realini
 %----------------------------------------------------------------------------------------------
 
+deltat = julday - 2444244.5;
+week = floor(deltat/7);
+sec_of_week = (deltat - week*7)*86400;
 
-a = floor(julday+.5);
-b = a+1537;
-c = floor((b-122.1)/365.25);
-e = floor(365.25*c);
-f = floor((b-e)/30.6001);
-d = b-e-floor(30.6001*f)+rem(julday+.5,1);
-day_of_week = rem(floor(julday+.5),7);
-week = floor((julday-2444244.5)/7);
-% We add +1 as the GPS week starts at Saturday midnight
-sec_of_week = (rem(d,1)+day_of_week+1)*86400;
