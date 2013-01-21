81b3d156d6260338d0849727ed400c43714f7629
ege010 <ege010@08326c51-6061-44f8-bb4b-f620d3b48b87>
2013-01-21 04:32:28 +0000

    Now the SP3 file loader checks if all the required SP3 files are available before trying to do the processing.

--- trunk/input_output/SP3/load_SP3.m
+++ trunk/input_output/SP3/load_SP3.m
@@ -109,65 +109,75 @@ SP3_coor = zeros(3,32,nEpochs);
 SP3_clck = zeros(32,nEpochs);
 
 k = 0;
+flag_unavail = 0;
 
 for p = 1 : size(week_dow,1)
     
     %open SP3 file
     f_sp3 = fopen([filename_SP3 num2str(week_dow(p,1)) num2str(week_dow(p,2)) '.sp3'],'r');
     
-    %skip the SP3 header (first 22 lines)
-    for i = 1 : 22
-        fgetl(f_sp3);
-    end
-
-    while (~feof(f_sp3))
+    if (f_sp3 ~= -1)
+        %skip the SP3 header (first 22 lines)
+        for i = 1 : 22
+            fgetl(f_sp3);
+        end
         
-        %get the next line
-        lin = fgetl(f_sp3);
-        
-        if (strcmp(lin(1),'*'))
+        while (~feof(f_sp3))
             
-            k = k + 1;
+            %get the next line
+            lin = fgetl(f_sp3);
             
-            %read the epoch header
-            %example 1: "*  1994 12 17  0  0  0.00000000"
-            data = textscan(lin(2:31),'%f%f%f%f%f%f');
-            year = data{1};
-            month = data{2};
-            day = data{3};
-            hour = data{4};
-            minute = data{5};
-            second = data{6};
-            %computation of the corresponding julian day
-            jd = julday(year, month, day, 0);
-            %computation of the GPS time in weeks and seconds of week
-            [week, sec_of_week] = gps_time(jd); %#ok<ASGLU>
-            SP3_time(k,1) = sec_of_week + hour*3600+minute*60+second;
-            
-        elseif (strcmp(lin(1),'P'))
-            %read position and clock
-            %example 1: "P  1  16258.524750  -3529.015750 -20611.427050    -62.540600"
-            %example 2: "PG01   8953.350886  12240.218129 -21918.986611 999999.999999"
-            %example 3: "PG02 -13550.970765 -16758.347434 -15825.576525    274.198680  7  8  8 138"
-            if (strcmp(lin(2),'G') | strcmp(lin(2),' '))
-                PRN = sscanf(lin(3:4),'%f');
-                X   = sscanf(lin(5:18),'%f');
-                Y   = sscanf(lin(19:32),'%f');
-                Z   = sscanf(lin(33:46),'%f');
-                clk = sscanf(lin(47:60),'%f');
+            if (strcmp(lin(1),'*'))
                 
-                SP3_coor(1,PRN,k) = X*1e3;
-                SP3_coor(2,PRN,k) = Y*1e3;
-                SP3_coor(3,PRN,k) = Z*1e3;
+                k = k + 1;
                 
-                SP3_clck(PRN,k) = clk/1e6; %NOTE: clk >= 999999 stands for bad or absent clock values
+                %read the epoch header
+                %example 1: "*  1994 12 17  0  0  0.00000000"
+                data = textscan(lin(2:31),'%f%f%f%f%f%f');
+                year = data{1};
+                month = data{2};
+                day = data{3};
+                hour = data{4};
+                minute = data{5};
+                second = data{6};
+                %computation of the corresponding julian day
+                jd = julday(year, month, day, 0);
+                %computation of the GPS time in weeks and seconds of week
+                [week, sec_of_week] = gps_time(jd); %#ok<ASGLU>
+                SP3_time(k,1) = sec_of_week + hour*3600+minute*60+second;
+                
+            elseif (strcmp(lin(1),'P'))
+                %read position and clock
+                %example 1: "P  1  16258.524750  -3529.015750 -20611.427050    -62.540600"
+                %example 2: "PG01   8953.350886  12240.218129 -21918.986611 999999.999999"
+                %example 3: "PG02 -13550.970765 -16758.347434 -15825.576525    274.198680  7  8  8 138"
+                if (strcmp(lin(2),'G') | strcmp(lin(2),' '))
+                    PRN = sscanf(lin(3:4),'%f');
+                    X   = sscanf(lin(5:18),'%f');
+                    Y   = sscanf(lin(19:32),'%f');
+                    Z   = sscanf(lin(33:46),'%f');
+                    clk = sscanf(lin(47:60),'%f');
+                    
+                    SP3_coor(1,PRN,k) = X*1e3;
+                    SP3_coor(2,PRN,k) = Y*1e3;
+                    SP3_coor(3,PRN,k) = Z*1e3;
+                    
+                    SP3_clck(PRN,k) = clk/1e6; %NOTE: clk >= 999999 stands for bad or absent clock values
+                end
             end
         end
+        
+        %close SP3 file
+        fclose(f_sp3);
+    else
+        fprintf('Missing SP3 file: %s\n', [filename_SP3 num2str(week_dow(p,1)) num2str(week_dow(p,2)) '.sp3']);
+        flag_unavail = 1;
     end
-    
-    %close SP3 file
-    fclose(f_sp3);
-    
+end
+
+%if the required SP3 files are not available, stop the execution
+if (flag_unavail)
+    error('Error: required SP3 files not available.');
 end
 
 %remove empty slots
