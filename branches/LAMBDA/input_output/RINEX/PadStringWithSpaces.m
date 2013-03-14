cffdc2ac9a0d9a8647f27559bb2416fb64facefa
climbercat <climbercat@08326c51-6061-44f8-bb4b-f620d3b48b87>
2013-03-14 11:16:17 +0000

    [branches/LAMBDA] removed (now it's included in goD)

--- branches/LAMBDA/input_output/RINEX/PadStringWithSpaces.m
+++
@@ -1,6 +0,0 @@
-function [ String ] = PadStringWithSpaces( String, RequestedLengthOfPaddedString )
-    NumberOfSpacesToAdd = RequestedLengthOfPaddedString - length(String);
-    if NumberOfSpacesToAdd > 0
-        String = [String, repmat(' ',1,NumberOfSpacesToAdd)];
-    end %if
-end
