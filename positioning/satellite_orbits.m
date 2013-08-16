db614893ef97a4da0da48363fd70f56dfffd75e9
ege010 <eugenio.realini@gmail.com>
2013-08-16 09:02:56 +0000

    [trunk] Changed two file headers

--- positioning/satellite_orbits.m
+++ positioning/satellite_orbits.m
@@ -21,8 +21,20 @@ function [satp, satv] = satellite_orbits(t, Eph, sat, sbas)
 %                           goGPS v0.4.1 beta
 %
 % Copyright (C) 2009-2013 Mirko Reguzzoni, Eugenio Realini
+%----------------------------------------------------------------------------------------------
 %
-% Partially based on SATPOS.M (EASY suite) by Kai Borre
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %----------------------------------------------------------------------------------------------
 
 switch char(Eph(31))
