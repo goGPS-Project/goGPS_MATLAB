877b43eeb3b65feb572288862af594b0a6182279
born-to-fly <born-to-fly@08326c51-6061-44f8-bb4b-f620d3b48b87>
2014-04-15 08:02:08 +0000

    Fix wrong number of sat_slip\sat_born in case of an empty list

--- trunk/positioning/ambiguity_init.m
+++ trunk/positioning/ambiguity_init.m
@@ -68,10 +68,10 @@ N_stim_slip = [];
 N_stim_born = [];
 
 %number of slipped satellites
-nsat_slip = size(sat_slip,1);
+nsat_slip = length(sat_slip);
 
 %number of new satellites
-nsat_born = size(sat_born,1);
+nsat_born = length(sat_born);
 
 %merge new and slipped satellites
 sat_amb = [sat_slip; sat_born];
