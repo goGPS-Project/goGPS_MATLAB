1b207af9ea8b6a8eae7396676708ff66133f8202
born-to-fly <born-to-fly@08326c51-6061-44f8-bb4b-f620d3b48b87>
2014-05-02 17:12:11 +0000

    Reverse wrong changes in finding satellite vector

--- trunk/positioning/kalman/goGPS_KF_DD_code_phase_init.m
+++ trunk/positioning/kalman/goGPS_KF_DD_code_phase_init.m
@@ -150,11 +150,13 @@ if (length(phase) == 2)
                 (pr2_R ~= 0) & (pr2_M ~= 0) & (ph2_R ~= 0) & (ph2_M ~= 0) );
 else
     if (phase == 1)
-        sat_pr = find( (pr1_R ~= 0) & (pr1_M ~= 0) );
-        sat = find( (pr1_R ~= 0) & (pr1_M ~= 0) );
+        sat_pr = find( (pr1_R ~= 0) & (pr1_M ~= 0) );        
+        sat = find( (pr1_R ~= 0) & (pr1_M ~= 0) & ...
+                    (ph1_R ~= 0) & (ph1_M ~= 0) );
     else
         sat_pr = find( (pr2_R ~= 0) & (pr2_M ~= 0) );
-        sat = find( (pr2_R ~= 0) & (pr2_M ~= 0) );
+        sat = find( (pr2_R ~= 0) & (pr2_M ~= 0) & ...
+                    (ph2_R ~= 0) & (ph2_M ~= 0) );
     end
 end
 sat_pr = sat_pr(ismember(sat_pr, Eph(30,:)));
