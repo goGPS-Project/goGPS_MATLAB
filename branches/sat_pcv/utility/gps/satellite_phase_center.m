5d0bd6815b9e58bb76496e548d18d65631c228db
ege010 <ege010@08326c51-6061-44f8-bb4b-f620d3b48b87>
2014-08-01 16:14:32 +0000

    [branches/sat_pcv] Added a specific function that computes sun and moon position at a given date (sun_moon_pos.m); bug-fixed the previous code to compute the sun position

--- branches/sat_pcv/utility/gps/satellite_phase_center.m
+++ branches/sat_pcv/utility/gps/satellite_phase_center.m
@@ -2,9 +2,11 @@ function [XS_pc, Az, El, D, time] = satellite_phase_center(XS_cm)
 
 global iephem km ephname inutate psicor epscor ob2000
 
-% compute the Sun position (ICRS)
+% compute the Sun position
 sun_id = 11; earth_id = 3;
 readleap; iephem = 1; ephname = 'de421.bin'; km = 1; inutate = 1; ob2000 = 0.0d0;
+setmod(2);
+setdt(3020092e-7);
 
 %if the binary JPL ephemeris file is not available, generate it
 if (~exist('./utility/sun_moon/jpl_ephem/de421.bin','file'))
@@ -19,28 +21,28 @@ year = 2014;
 month = 7;
 
 i = 1;
-for day = 1 : 0.01 : 2
+for day = 1 : 0.001 : 2
     jdutc = julian(month, day, year);
     jdtdb = utc2tdb(jdutc);
     rrd = jplephem(jdtdb, sun_id, earth_id);
     
-    %ICRS coordinates (ecliptic)
-    sun_ECEF = rrd(1:3);
+    %ICRS coordinates
+    sun_ECI = rrd(1:3);
+    tmatrix = j2000_icrs(1);
+    sun_ECI = tmatrix*sun_ECI;
     
-    %ICRS ecliptic to equatorial coordinates
-    sun_ECEF = eceq(0.0d0, 0, sun_ECEF);
-    
-    %precise celestial pole (now disabled)
+    %precise celestial pole (disabled)
     [psicor, epscor] = celpol(jdtdb, 1, 0.0d0, 0.0d0);
     
-    %ICRS equatorial coordinates to ITRS equatorial coordinates
-    xp = 0.0d0; yp = 0.0d0;
-    tjdh = floor(jdutc); tjdl = jdutc - tjdh;
-    setdt(64);
-    TRF(:,i) = celter(tjdh, tjdl, xp, yp, sun_ECEF);
+    %ICRS coordinates to ITRS coordinates
+    xp = 171209e-6; yp = 414328e-6;
+    deltat = getdt;
+    jdut1 = jdutc - deltat;
+    tjdh = floor(jdut1); tjdl = jdut1 - tjdh;
+    sun_ECEF(:,i) = celter(tjdh, tjdl, xp, yp, sun_ECI);
     
     %topocentric coordinates
-    [Az(i), El(i), D(i)] = topocent([4.398972273573140e+06; 7.006644334305141e+05; 4.549920877913681e+06], TRF(:,i)');
+    [Az(i), El(i), D(i)] = topocent([4.398834576291538e+06; 7.006425011286493e+05; 4.549777495792151e+06], sun_ECEF(:,i)'*1e3);
     
     [gps_week, gps_sow] = jd2gps(jdutc);
     gps_date = gps2date(gps_week, gps_sow);
@@ -49,4 +51,4 @@ for day = 1 : 0.01 : 2
     i = i + 1;
 end
 
-XS_pc = TRF;
+XS_pc = sun_ECEF;
