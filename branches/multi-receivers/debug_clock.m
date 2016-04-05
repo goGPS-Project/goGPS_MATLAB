33240f84ab3d6fcdf69b2b9ccadd9a53fb002d14
ege010 <ege010@08326c51-6061-44f8-bb4b-f620d3b48b87>
2016-04-05 10:18:06 +0000

    [branches] Removing some old unused branches (the code of each one is stored locally on our computers; if anyone is interested, just ask!)

--- branches/multi-receivers/debug_clock.m
+++
@@ -1,9 +0,0 @@
- close all
-figure
-plot(dtR(:,1,1))
-
-
-figure
-plot(dtR(:,1,1)-(time_R(:,:,1)-time_GPS),'*b')
-hold on
-plot(((time_R(:,:,1)-time_GPS))*(( max(abs((time_R(:,:,1)-time_GPS)))/max(abs((dtR(:,1,1)-(time_R(:,:,1)-time_GPS))))))^-1,'-r')
