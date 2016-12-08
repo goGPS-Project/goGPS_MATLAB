3ad5952ce64997fc54b8e322f20cf968973d83c6
clicat <gatti.andrea@gmail.com>
2016-12-09 00:13:21 +0100

    Speedup of OLOO, now is 10% to 90% faster
    The new OLOO implementation takes into account the sparse condition of the inputs, the speedup ranges from 10% for small matrixes (tipical condition of the kalman filter approach) to 90% if applyied on bigger matrixes (~9000 parameters, tipical condition of a big LS solution)

--- goGPS/utility/OLOO.m
+++ goGPS/utility/OLOO.m
@@ -3,8 +3,10 @@ function [imax,xfin,s2fin,ufin,Cxx,uout]=OLOO(A, y, Q)
 %           identify one (block) outlier
 %           reject it
 %           re-estimate unknowns
-%           according to the theory in "L. Biagi and S. Caldera.An efficient leave one block out approach to identify outliers.Journal of Applied Geodesy, Volume 7, Issue 1, pages 11–19, 2013"
+%           according to the theory in "L. Biagi and S. Caldera.An efficient leave one block out approach to identify outliers.Journal of Applied Geodesy, Volume 7, Issue 1, pages 11?19, 2013"
 %1.0: Stefano Caldera, 22.05.2014
+%1.1: Stefano Caldera, Andrea Gatti 08.12.2016
+%           speedup improvements by Andrea Gatti
 %input:
 %   A: design matrix
 %   y: observations vector
@@ -22,123 +24,122 @@ function [imax,xfin,s2fin,ufin,Cxx,uout]=OLOO(A, y, Q)
 
 
 %%
-global FTABLE;
+% when FTABLE is undefined, redefine it
+global FTABLE; if isempty(FTABLE); FTABLE = finv(0.9995,1,1:size(A,1))'; end
+if (size(FTABLE,1) < size(A,1)); FTABLE = finv(0.9995,1,1:size(A,1))'; end
+n_blocks = length(y);
+[m, n] = size(A);           % m: number of observations, n: number of unknowns
+uout = NaN;
 
-n_blocks=length(y);
-[m,n]=size(A);           % m: number of observations, n: number of unknowns
-uout=NaN;
 if m - n > 0
    
-    %% compute the global solution
+    % compute the global solution
+    if isdiag(Q) % Q is tipically diagonal -> let's use this information
+        Qm = diag(Q);
+        Qm = Qm ./ min(Qm);
+        invQ = diag(1./Qm);
+        At_invQ = A' * invQ;
+        Ninv = inv(At_invQ * A);
+        xcap = Ninv * At_invQ * y;
+        um = y - A * xcap;
+        s2cap = um' * invQ * um/(m-n);
+    else        
+        Q = Q ./ (min(diag(Q)));
+        At_invQ = A'/Q;
+        Ninv = inv(At_invQ * A);
+        xcap = Ninv * At_invQ * y;
+        um = y - A * xcap;
+        s2cap = um' / Q * um/(m-n);
+        Qm = diag(Q);
+    end
+    % convert A in a sparse matrix, faster and lighter
     
-    Q=Q./(min(diag(Q)));
-    invQ=inv(Q);
-    Ninv=inv(A'*invQ*A);
-    xcap=Ninv*A'*invQ*y;
-    um=y-A*xcap;
-    s2cap=um'*invQ*um/(m-n);
+    use_sparse_approach = false;
     
-    Qm=diag(Q);
-    
+    if sum(A(1,:)==0) > size(A,2) / 0.577     % if A is sparse enough
+        use_sparse_approach  = true;
+        A = sparse(A);        
+    end
     if m - n > 1
         %% start outliers rejection
-        Im=eye(n);
-        Bm=Ninv*A';
-        Cm=diag(A*Bm);
-        Km=Qm-Cm;
-        Kminv=Km.^-1;
-        wm=Qm.*Kminv.*um;
-        s2m=((m-n).*s2cap-um.*Kminv.*um)./(m-n-1);      
-
-%         keyboard
+        Im = eye(n);
+        Bm = Ninv * A';
+        Cm = diag(A * Bm);
+        Km = Qm - Cm;
+        Kminv = Km.^-1;
+        wm = Qm .* Kminv .* um;
+        s2m = ((m-n) .* s2cap - um .* Kminv .* um) ./ (m - n - 1);
         
-%         clear temp
-%         tic               
-%         temp=[];
-%         for i=1:n_blocks
-%            temp= blkdiag(temp,Bm(:,i)');             
-%         end
-%         toc
-%         %[Bm',zeros((size(Bm',1)-1)*size(Bm',2),size(Bm',1))'];
-%         
-%         tic
-%         D=reshape([Bm',zeros((size(Bm',1))*size(Bm',2),size(Bm',1))']',[],1)';
-%         D=reshape(D(1:end-(size(Bm',1))*size(Bm',2)),(size(Bm',1))*size(Bm',2),size(Bm,2))';
-%         Qw1=D*(eye(n*m)+reshape(A',[],1).*reshape(repmat(Kminv,1,4)',[],1)*Bm(:)');
-%         toc
-%         
-%         %Qw2=kron(eye(n_blocks),Bm')*(eye(n*m)+reshape(A',[],1).*reshape(repmat(Kminv,1,4)',[],1)*Bm(:)');
-%         %toc
-% tic
-        Qw=zeros(n_blocks,1);
-        for i=1:n_blocks
-            Qw(i)=Qm(i)+Bm(:,i)'*(Im+A(i,:)'*Kminv(i)*Bm(:,i)')*A(i,:)';
-            %Qw3=Bm(:,i)'*(Im+A(i,:)'*Kminv(i)*Bm(:,i)');
+        % original loop
+        if use_sparse_approach
+            % modified loop to exploit the sparse property of the A matrix
+            Qw = zeros(n_blocks,1);
+            tmp = (A .* repmat(Kminv,1,n));
+            for i = 1 : n_blocks
+                idOk = (tmp(i,:) ~= 0);
+                Qw(i) = Qm(i) + Bm(idOk,i)' * (Im(idOk,idOk) + tmp(i,idOk)' * Bm(idOk,i)')  * A(i,idOk)';
+            end
+        else
+            Qw = zeros(n_blocks,1);
+            for i = 1 : n_blocks
+                Qw(i) = Qm(i) + Bm(:,i)' * (Im + A(i,:)' * Kminv(i) * Bm(:,i)') * A(i,:)';
+                %Qw3=Bm(:,i)'*(Im+A(i,:)'*Kminv(i)*Bm(:,i)');
+            end
         end
-%toc
-        Qwinv=Qw.^-1;
-        deg2=m-n-1;
+
+        %toc
+        Qwinv = Qw.^-1;
+        deg2 = m - n - 1;
         
-        F=wm.*Qwinv.*wm./s2m;        
-        Flim=FTABLE(deg2);
+        F = wm .* Qwinv .* wm ./ s2m;
+        Flim = FTABLE(deg2);
         
         
         %% apply final solution
         % find maximum F(i)/Flim(i)
-        [Fmax,imax]=max(abs(F./Flim));
-
-        if (Fmax<1)
+        [Fmax, imax] = max(abs(F ./ Flim));
+        
+        if (Fmax < 1)
             % no outlier
-            imax=0;
-            xfin=xcap;
-            %yfin=y;
-            %Afin=A;
-            %Qfin=Q;
-            s2fin=s2cap;
-            Ninvfin=Ninv;
-            ufin=um;            
-            Cxx=s2fin*Ninvfin;
+            imax = 0;
+            xfin = xcap;
+            s2fin = s2cap;
+            Ninvfin = Ninv;
+            ufin = um;
+            Cxx = s2fin * Ninvfin;
             
         else
             % if the maximum ratio exceedes the threshold, the observation is eliminated from the solution
-            uout=um(imax);
-            xfin=xcap-Bm(:,imax)*Kminv(imax)*um(imax);
-            yfin=y;
-            yfin(imax)=[];
-
-            Afin=A;
-            Afin(imax,:)=[];
+            uout = um(imax);
+            xfin = xcap - Bm(:, imax) * Kminv(imax) * um(imax);
+            yfin = y;
+            yfin(imax) = [];
             
-            %Qfin=Q;
-            %Qfin(imax,:)=[];
-            %Qfin(:,imax)=[];
+            Afin = A;
+            Afin(imax,:) = [];
+                        
+            s2fin = s2m(imax);
             
-            s2fin=s2m(imax);
+            Ninvfin = Ninv + Bm(:, imax) * Kminv(imax) * Bm(:, imax)';
+            ufin = yfin - Afin * xfin;
+            Cxx = s2fin * Ninvfin;
             
-            Ninvfin=Ninv+Bm(:,imax)*Kminv(imax)*Bm(:,imax)';
-            ufin=yfin-Afin*xfin;
-            Cxx=s2fin*Ninvfin;
-
         end
-
+        
     else
-        imax=0;
-        xfin=xcap;
-        %yfin=y;
-        %Afin=A;
-        %Qfin=Q;
-        s2fin=s2cap;
-        Cxx=s2cap*Ninv;
-        ufin=um;
+        imax = 0;
+        xfin = xcap;
+        s2fin = s2cap;
+        Cxx = s2cap * Ninv;
+        ufin = um;
     end
     
 else
     % detection is not possibile
-    imax=0;
-    xfin=[];
-    Cxx=[];
-    %yfin=[];
-    ufin=[];
-    s2fin=NaN;
+    imax = 0;
+    xfin = [];
+    Cxx = [];
+    ufin = [];
+    s2fin = NaN;
 end
 
