3393ec48a69c95c92933f9e7effde96f114e8a1a
clicat <gatti.andrea@gmail.com>
2017-08-20 18:56:31 +0200

    Minor improvements on sourceCleaner

--- goGPS/utility/sourceCleaner.m
+++ goGPS/utility/sourceCleaner.m
@@ -1,4 +1,4 @@
-function sourceCleaner(base_dir)
+function sourceCleaner(base_dir, rem_spaces)
 % SYNTAX:
 %    sourceCleaner( base_dir);
 % EXAMPLE:
@@ -39,12 +39,24 @@ function sourceCleaner(base_dir)
 %--------------------------------------------------------------------------
 
 % find all the m files in goGPS directory
-if (nargin <= 1)
+if (nargin < 1)
     base_dir = '.';
 end
-[~, list] = dos(['find ' base_dir ' -name \*.m']);
-list = textscan(list,'%s','Delimiter','\n','whitespace','');
-list = list{1};
+if (nargin < 2)
+    rem_spaces = false;
+end
+
+if exist(base_dir, 'file')
+    if iscell(base_dir)
+        list = base_dir;
+    else
+        list = {base_dir};
+    end
+else
+    [~, list] = dos(['find ' base_dir ' -name \*.m']);
+    list = textscan(list,'%s','Delimiter','\n','whitespace','');
+    list = list{1};
+end
 tic
 for i = 1 : length(list)
     file_name = list{i};
@@ -52,7 +64,11 @@ for i = 1 : length(list)
     fid = fopen(file_name, 'r');
     txt = fread(fid,'*char')';
     fclose(fid);
-    clean_txt = regexprep(txt,'([ |\t|\r]*(?=\n))','');
+    clean_txt = regexprep(txt,'(?<=[A-Za-z]|\%|;|\)|[0-9])([ |\t|\r]*(?=\n))','');
+    if rem_spaces
+        clean_txt = regexprep(clean_txt,'(?<=\n)([ |\t|\r]*(?=[A-Za-z]|\%))','');
+    end
+    
     if not(isempty(clean_txt))
         fid = fopen(file_name, 'w');
         fwrite(fid, clean_txt);
