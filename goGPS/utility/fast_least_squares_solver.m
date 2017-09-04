047eb5894771f97cabb2d9b42d3106e9bbcc4958
clicat <gatti.andrea@gmail.com>
2017-09-04 10:00:21 +0200

    Minor speed improvements

--- goGPS/utility/fast_least_squares_solver.m
+++ goGPS/utility/fast_least_squares_solver.m
@@ -1,28 +1,31 @@
-function [x, Cxx, sigma02_hat, v_hat, Cyy] = fast_least_squares_solver(y0, b, A, Q)
+function [x, Cxx, s02_hat, v_hat, Cyy] = fast_least_squares_solver(y0, b, A, Q)
 
 [n, m] = size(A);
 
-%least-squares solution
+% least-squares solution
 P = A' / Q;
 N = P * A;
-Y = (y0-b);
-L = P * Y;
-x = N\L;
 
-%estimation of the variance of the observation error
-y_hat = A * x + b;
-v_hat = y0 - y_hat;
-V = v_hat';
-T = Q \ v_hat;
-sigma02_hat = (V * T) / (n - m);
-
-%covariance matrix
 try
-    Cxx = sigma02_hat * cholinv(N);
+    N_inv = cholinv(full(N));
 catch
-    Cxx = sigma02_hat * N^-1;
+    N_inv = N^-1;
 end
 
+Y = (y0 - b);
+L = P * Y;
+x = N_inv * L;
+
+% estimation of the variance of the observation error
+y_hat = A * x + b;
+v_hat = y0 - y_hat;
+T = Q \ v_hat;
+s02_hat = (v_hat' * T) / (n - m);
+
+% covariance matrix
+Cxx = s02_hat * N_inv;
+
 if nargout == 5
-    Cyy = sigma02_hat * A/N*A';
+    A = A';
+    Cyy = s02_hat * A' * N_inv * A;
 end
