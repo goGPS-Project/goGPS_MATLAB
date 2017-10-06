a5e10cec12731a9f2c7ffbf7a21c824a55f52d8a
giuliot <giulio.tagliaferro@g-red.eu>
2017-10-06 10:23:28 +0200

    LS empty object

---
+++ goGPS/obj/LS.m
@@ -0,0 +1,15 @@
+classdef LS < handle
+    properties 
+        A 
+        y0
+        b
+        Q
+    end
+    methods
+        function this = LS()
+        end
+        function [x] = solve(this)
+            %TBD
+        end
+    end
+end
