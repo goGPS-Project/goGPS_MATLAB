824fd5610e276679073b44b79bd9ab9d971ba9fd
giuliot <giulio.tagliaferro@g-red.eu>
2017-12-19 19:24:15 +0100

    👾 Bug fix: connect sinex DCB

--- goGPS/utility/idxCharLines.m
+++ goGPS/utility/idxCharLines.m
@@ -1,5 +1,16 @@
 function idx = idxCharLines(mat,line)
 %DESCRIPTION: given a matrix of char (mat) return the index of the lines
 %equal to line
- idx = sum(mat == repmat(line,size(mat,1),1) ,2) == size(mat,2);
+n_idx = size(line,1);
+if n_idx == 1
+    idx= sum(mat == repmat(line,size(mat,1),1) ,2) == size(mat,2); % to be backward compatible
+else
+    idx = zeros(n_idx,1);
+    for i = 1: length(idx)
+        idx_t = find(sum(mat == repmat(line(i,:),size(mat,1),1) ,2) == size(mat,2));
+        if ~isempty(idx_t)
+            idx(i) = idx_t;
+        end
+    end
+end
 end
