2a9c2080d856930a53263a9bb25b0bf484d3ee8f
clicat <gatti.andrea@gmail.com>
2017-12-22 09:47:46 +0100

    Adding clearBranch 🧟‍ to utilities
    script to be executed after the change of a branch, to avoid zombies and dangerous settings

---
+++ goGPS/utility/clearBranch.m
@@ -0,0 +1,4 @@
+% script to clean the workspace after a branch change
+clear all
+clear GO_Settings Logger Main_Settings Settings_Interface Main_Settings Mode_Settings Constellation_Collector IO_Settings Core Core_Sky Receiver
+delete last_settings.ini
