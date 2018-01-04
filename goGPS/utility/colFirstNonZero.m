f23249d98b4c127057f1e9d047f7a511544ec9a0
clicat <gatti.andrea@gmail.com>
2018-01-04 09:51:20 +0100

    tmp commit -> working on Giulio's computer

--- goGPS/utility/colFirstNonZero.m
+++ goGPS/utility/colFirstNonZero.m
@@ -1,6 +1,8 @@
 function B = colFirstNonZero(A)
-%DESCRIPTION: get first non zero element of each column of a matrix
-[~, c] = max( A ~=0, [], 1 );
-d = (0:(size(A,2)-1))*size(A,1) +c;
-B=A(d);
+    % Get first non zero element of each column of a matrix
+    % SYNTAX:
+    % B = colFirstNonZero(A)
+    [~, c] = max( A ~=0, [], 1 );
+    d = (0 : (size(A, 2) - 1)) * size(A, 1) + c;
+    B = A(d);
 end
