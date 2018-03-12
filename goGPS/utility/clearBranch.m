85113f7d05404d428d5dd312812a7fa31de668b8
clicat <gatti.andrea@gmail.com>
2018-03-12 15:36:48 +0100

    Update cleanin with the new classes

--- goGPS/utility/clearBranch.m
+++ goGPS/utility/clearBranch.m
@@ -1,4 +1,4 @@
 % script to clean the workspace after a branch change
 clear all
-clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector IO_Settings Core Core_Sky Receiver
-delete last_settings.ini
+clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky Receiver Global_Configuration Command_Interpreter
+% delete last_settings.ini
