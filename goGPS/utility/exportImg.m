5e6696e957fdced32a68bc7973e0b5e5e2870b4d
clicat <gatti.andrea@gmail.com>
2018-04-23 15:59:10 +0200

    Adding support for 1bit masks

--- goGPS/utility/exportImg.m
+++ goGPS/utility/exportImg.m
@@ -36,17 +36,22 @@ function exportImg(file_name, data_map, c_map)
 %--------------------------------------------------------------------------
 
     narginchk(2,3);
-    if nargin < 3
-        c_map = jet(256);
-    end
-        
-    % Normalize data
-    im_alpha = isnan(data_map);
-    data_map = data_map - min(data_map(:)); 
-    data_map = max(2, ceil(data_map ./ max(data_map(:)) .* (size(c_map, 1) -1)) + 1);
-    data_map(im_alpha) = 1;    
-    c_map = [[0, 0, 0]; c_map];
     
-    im = reshape([c_map(data_map(:), 1); c_map(data_map(:), 2); c_map(data_map(:), 3)], size(data_map, 1), size(data_map, 2), 3);
-    imwrite(im, file_name, 'Alpha', 1-im_alpha);
+    if islogical(data_map)
+        imwrite(data_map, file_name);
+    else
+        if nargin < 3
+            c_map = jet(256);
+        end
+        
+        % Normalize data
+        im_alpha = isnan(data_map);
+        data_map = data_map - min(data_map(:));
+        data_map = max(2, ceil(data_map ./ max(data_map(:)) .* (size(c_map, 1) -1)) + 1);
+        data_map(im_alpha) = 1;
+        c_map = [[0, 0, 0]; c_map];
+        
+        im = reshape([c_map(data_map(:), 1); c_map(data_map(:), 2); c_map(data_map(:), 3)], size(data_map, 1), size(data_map, 2), 3);
+        imwrite(im, file_name, 'Alpha', 1-im_alpha);
+    end
 end
