f00761995dbbb1319454941220ce660d2cf4da7c
clicat <gatti.andrea@gmail.com>
2018-05-30 11:50:17 +0200

    Updating clearBranch  (no more warnings)

--- goGPS/utility/clearBranch.m
+++ goGPS/utility/clearBranch.m
@@ -1,4 +1,6 @@
 % script to clean the workspace after a branch change
+warning('off');
 clear all
-clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky Receiver Global_Configuration Command_Interpreter
+clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky Receiver Global_Configuration Command_Interpreter Meteo_Network Athmosphere
 % delete last_settings.ini
+warning('off');
