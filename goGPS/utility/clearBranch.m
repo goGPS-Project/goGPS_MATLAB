9be73f86100e3f148019f3711da514b7b81d2d16
giuliot <giulio.tagliaferro@g-red.eu>
2018-06-15 15:24:09 +0200

    Update clearBranch

--- goGPS/utility/clearBranch.m
+++ goGPS/utility/clearBranch.m
@@ -1,6 +1,7 @@
 % script to clean the workspace after a branch change
 warning('off');
 clear all
-clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky Receiver Global_Configuration Command_Interpreter Core_Reference_Frame Meteo_Network Athmosphere Remote_Resource_Manager
+clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky ...
+    Receiver_Work_Space Receiver_Output Receiver_Commons GNSS_Station Global_Configuration Command_Interpreter Core_Reference_Frame Meteo_Network Athmosphere Remote_Resource_Manager
 % delete last_settings.ini
 warning('off');
