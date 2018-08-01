8c311bcb41282962fc36ba99fc5d0c5bcdec4981
giuliot <giulio.tagliaferro@g-red.eu>
2018-08-01 16:31:24 +0200

    epn tropo check bug fix

--- goGPS/scripts/epn_tropo_check.m
+++ goGPS/scripts/epn_tropo_check.m
@@ -2,7 +2,7 @@
 project_path = '../../remote_data/project/EPN_test_set/station/';
 %goGPS([project_path '/config/config.ini'],false);
 getResults();
-solutions = {'asi19751.tro'  'bek19751.tro'  'bkg19751.tro' 'eur19757.tro'};
+solutions = {'asi19752.tro'  'bek19751.tro'  'bkg19751.tro' 'eur19757.tro'};
 ext_sol = parseMultiStationTropoSinex([project_path solutions{1}]);
 mode = 'interpolate', % possible values : 'aggregate', 'interpolate'
 ztd_diff = [];
@@ -14,9 +14,10 @@ for r = 1 : max(size(rec))
     sta_code = rec(r).getMarkerName4Ch;
     if isfield(ext_sol,sta_code)
         res = ext_sol.(sta_code);
-        t1 = res.time.getGpsTime();
+        t1 = res.time.getMatlabTime();
         [ztd] = rec(r).out.getZtd();
-        t2 = rec(r).out.getTime().getGpsTime();
+        t2 = rec(r).out.getTime().getMatlabTime();
+        
         idx_el = t1 < min(t2) | t1 > max(t2);
         t1(idx_el) = [];
         res.TROTOT(idx_el) = [];
@@ -33,18 +34,20 @@ for r = 1 : max(size(rec))
         end
         sta_id = [sta_id; r*ones(size(ztd_diff_t))];
         xyz_diff = [xyz_diff; res.xyz - rec(r).out.xyz(1)];
-%         figure
-%         plot(t1,res.TROTOT/1000)
-%         title(['ZTD ' sta_code]);
-%         hold on; plot(t2,ztd,'.')        
+        figure
+        plot(t1,res.TROTOT/1000)
+        title(['ZTD ' sta_code]);
+        hold on; plot(t2,ztd,'.')        
         figure
         plot(t1,res.TGNTOT/1000)
         title(['TGN ' sta_code]);
         hold on; plot(t2,tgn,'.')
+         setTimeTicks(6,'dd/mm/yyyy HH:MMPM')
         figure
         plot(t1,res.TGETOT/1000)
         title(['TGE ' sta_code]);
         hold on; plot(t2,tge,'.')
+         setTimeTicks(6,'dd/mm/yyyy HH:MMPM')
 %         figure
 %         plot(ztd_diff(sta_id == r));
 %         title(['ZTD diff' sta_code]);
