da8aa5b28100254eb93027785e7b49e3b08f7f0e
clicat <gatti.andrea@gmail.com>
2018-08-17 18:00:29 +0200

    👾 Bug fix: trying to find a way to avoid MATLAB crash on showResiduals
    MATLAB continues to crash for an opengl problem (the code is not the cause of it but it triggers it)

--- goGPS/obj/Receiver_Commons.m
+++ goGPS/obj/Receiver_Commons.m
@@ -984,21 +984,19 @@ classdef Receiver_Commons < handle
             for r = numel(sta_list)                
                 work = sta_list(r);
                 
-                win = figure( 'Name', sprintf('%s goGPS Residuals', work.parent.getMarkerName4Ch), ...
-                    'Visible', 'off', ...
+                win = figure('Visible', 'on', ...
                     'NumberTitle', 'off', ...
-                    'Position', [0 0 1000 600]);
-                maximizeFig(win)
-                
+                    'units', 'normalized', ...
+                    'outerposition', [0 0 1 1]); % create maximized figure
+                win.Name = sprintf('%d - %s residuals', win.Number, work.parent.getMarkerName4Ch);
                 v_main = uix.VBoxFlex('Parent', win, ...
                     'Spacing', 5);
-                
+
                 % Axe with all the satellites
                 overview_box = uix.VBoxFlex('Parent', v_main, ...
                     'BackgroundColor', Core_UI.LIGHT_GRAY_BG);
                 
-                ax_all = axes('Parent', overview_box, 'Units', 'normalized');
-                
+                ax_all = axes('Parent', overview_box, 'Units', 'normalized');               
                 
                 % Single sat axes
                 n_sat = work.getMaxSat;
@@ -1025,9 +1023,12 @@ classdef Receiver_Commons < handle
                         'FontWeight', 'Bold', ...
                         'BackgroundColor', Core_UI.LIGHT_GRAY_BG);
                     ax_sat(s) = axes('Parent', single_sat(s));
-                    single_sat(s).Heights = [18, -1];
                 end
                 sat_grid.Heights = -ones(1, ceil(n_sat / 4));
+                for s = 1 : n_sat
+                    single_sat(s).Heights = [18, -1];
+                    drawnow
+                end
                 
                 %% fill the axes
                 win.Visible = 'on';
@@ -1044,7 +1045,7 @@ classdef Receiver_Commons < handle
                     if id_ok(s)
                         plot(ax_sat(s), work.time.getMatlabTime, res, '-', 'LineWidth', 2, 'Color', colors(s, :));
                         grid on; ax_sat(s).YMinorGrid = 'on'; ax_sat(s).XMinorGrid = 'on';
-                        setTimeTicks(ax_sat(s), 2,'HH:MMPM');
+                        %setTimeTicks(ax_sat(s), 2,'HH:MMPM');
                     else
                         single_sat(s).Visible = 'off';
                     end
