bedfefd7e67605a48fe773ada1b3de26c86cbf6c
giuliot <giulio.tagliaferro@g-red.eu>
2018-08-22 15:59:08 +0200

    Making sense of free netwrok choice

--- goGPS/obj/Network.m
+++ goGPS/obj/Network.m
@@ -92,7 +92,7 @@ classdef Network < handle
             this.ztd_gn = nan(n_time, n_rec);
             this.ztd_ge = nan(n_time, n_rec);
             
-            % --- fill the values in the network
+            % --- fill the correction values in the network
             for i = 1 : n_rec
                 % if all value in the receiver are set to nan initilaize them to zero
                 if sum(isnan(this.rec_list(i).work.ztd)) == length(this.rec_list(i).work.ztd)
@@ -102,73 +102,99 @@ classdef Network < handle
                 end
                 % for all paramter take the apriori in the receiver and sum the netwrok estimated correction
                 idx_rec = x(:,3) == i;
-                this.coo(i,:) = [x(x(:,2) == 1 & idx_rec,1) x(x(:,2) == 2 & idx_rec,1) x(x(:,2) == 3 & idx_rec,1)] + this.rec_list(i).work.xyz;
-                [idx_is, idx_pos] = ismembertol(this.rec_list(i).work.getTime.getGpsTime(), this.common_time.getGpsTime, 0.002, 'DataScale', 1);
-                idx_pos = idx_pos(idx_pos > 0);
+                coo = [x(x(:,2) == 1 & idx_rec,1) x(x(:,2) == 2 & idx_rec,1) x(x(:,2) == 3 & idx_rec,1)];
+                this.coo(i,:) = coo;
+                
                 clk = x(x(:,2) == ls.PAR_REC_CLK & idx_rec,1);
-                clk_rec = this.rec_list(i).work.getDt();
-                this.clock(idx_pos,i) = clk_rec(idx_is);
-                this.clock(~isnan(this.rec_time_indexes(:,i)),i) = this.clock(~isnan(this.rec_time_indexes(:,i)),i) + clk;
+                this.clock(~isnan(this.rec_time_indexes(:,i)),i) = clk;
                 
                 if this.state.flag_tropo
                     ztd = x(x(:,2) == ls.PAR_TROPO & idx_rec,1);
+                    this.ztd(~isnan(this.rec_time_indexes(:,i)),i) = ztd;
+                end
+                
+                if this.state.flag_tropo_gradient
+                    gn = x(x(:,2) == ls.PAR_REC_CLK & idx_rec,1);
+                    this.ztd_gn(~isnan(this.rec_time_indexes(:,i)),i) = gn;
+                    
+                    ge = x(x(:,2) == ls.PAR_REC_CLK & idx_rec,1);
+                    this.ztd_ge(~isnan(this.rec_time_indexes(:,i)),i) = ge;
+                end
+            end
+            
+            % ALL OF THIS MAKES NO SENSE TO ME (Andrea). Now it should ;) (Giulio)
+            %--- transform the result in the desired free network
+            
+            if ~isnan(idx_ref(1)) && ~(length(idx_ref) == 1 && idx_ref == 1)
+                
+                S = zeros(n_rec);
+                S(:, idx_ref) = - 1 / numel(idx_ref);
+                S = S + eye(n_rec);  % < - this should be an S trasform but i am not sure
+                % it is the paramter itself  the mean of the reference paramter
+                % it is in matrix form so it can be used in the future for variance covariance matrix of the coordinates
+                
+                % Applying the S transform I obtain the corrections with respect to the reference
+                this.coo(:,1) = S * this.coo(:,1);
+                this.coo(:,2) = S * this.coo(:,2);
+                this.coo(:,3) = S * this.coo(:,3);
+                
+                % apply the S transform to the epochwise parameters
+                for i = 1 : n_time
+                    id_present = ~isnan(this.clock(i,:));
+                    idx_ref_t = intersect(idx_ref, find(id_present));
+                    if isempty(idx_ref_t)
+                        S = nan;
+                    else
+                        n_rec_t = sum(id_present);
+                        S = zeros(n_rec_t);
+                        S(:,idx_ref) = - 1 / numel(idx_ref_t);
+                        S = S + eye(n_rec_t);
+                    end
+                    % clock
+                    this.clock(i,:) = (S*this.clock(i,:)')';
+                    % ztd
+                    if this.state.flag_tropo
+                        this.ztd(i,:) = (S*this.ztd(i,:)')';
+                    end
+                    % gradients
+                    if this.state.flag_tropo_gradient
+                        this.ztd_gn(i,:) = (S*this.ztd_gn(i,:)')';
+                        this.ztd_ge(i,:) = (S*this.ztd_ge(i,:)')';
+                    end
+                end
+            end
+            
+            
+            % --- add the apriori values
+            for i = 1 : n_rec
+                % if all value in the receiver are set to nan initilaize them to zero
+                if sum(isnan(this.rec_list(i).work.ztd)) == length(this.rec_list(i).work.ztd)
+                    this.rec_list(i).work.ztd(:) = 0;
+                    this.rec_list(i).work.tge(:) = 0;
+                    this.rec_list(i).work.tgn(:) = 0;
+                end
+                % for all paramter take the apriori in the receiver and sum the netwrok estimated correction
+                this.coo(i,:) = coo + this.rec_list(i).work.xyz;
+                %
+                [idx_is, idx_pos] = ismembertol(this.rec_list(i).work.getTime.getGpsTime(), this.common_time.getGpsTime, 0.002, 'DataScale', 1);
+                idx_pos = idx_pos(idx_pos > 0);
+                clk_rec = this.rec_list(i).work.getDt();
+                this.clock(idx_pos,i) = this.clock(idx_pos,i) + clk_rec(idx_is);
+                
+                if this.state.flag_tropo
                     ztd_rec = this.rec_list(i).work.getZtd();
-                    this.ztd(idx_pos,i) = ztd_rec(idx_is);
-                    this.ztd(~isnan(this.rec_time_indexes(:,i)),i) = this.ztd(~isnan(this.rec_time_indexes(:,i)),i) + ztd;
+                    this.ztd(idx_pos,i) = this.ztd(idx_pos,i) + ztd_rec(idx_is);
                 end
                 
                 if this.state.flag_tropo_gradient
                     [gn_rec, ge_rec] = this.rec_list(i).work.getGradient();
-                     
-                    gn = x(x(:,2) == ls.PAR_REC_CLK & idx_rec,1);
-                    this.ztd_gn(idx_pos,i) = gn_rec(idx_is);
-                    this.ztd_gn(~isnan(this.rec_time_indexes(:,i)),i) = this.ztd_gn(~isnan(this.rec_time_indexes(:,i)),i) + gn;
                     
-                    ge = x(x(:,2) == ls.PAR_REC_CLK & idx_rec,1);
-                    this.ztd_ge(idx_pos,i) = ge_rec(idx_is);
-                    this.ztd_ge(~isnan(this.rec_time_indexes(:,i)),i) = this.ztd_ge(~isnan(this.rec_time_indexes(:,i)),i) + ge;
+                    this.ztd_gn(idx_pos,i) = this.ztd_gn(idx_pos,i) + gn_rec(idx_is);
+                    
+                    this.ztd_ge(idx_pos,i) = this.ztd_ge(idx_pos,i) + ge_rec(idx_is);
                 end
             end
-            
-            % ALL OF THIS MAKES NO SENSE TO ME (Andrea)
-            % --- transform the result in the desired free network
-            % if ~isnan(idx_ref(1))
-            %     S = zeros(n_rec);
-            %     S(:, idx_ref) = - 1 / numel(idx_ref);
-            %     S = S + eye(n_rec);  % < - this should be an S trasform but i am not sure 
-            %                          % it is the paramter itself  the mean of the reference paramter
-            %                          % it is in matrix form so it can be used in the future for variance covariance matrix of the coordinates
-            % 
-            %     % Applying the S transform I obtain the corrections with respect to the reference
-            %     this.coo(:,1) = S * this.coo(:,1);
-            %     this.coo(:,2) = S * this.coo(:,2);
-            %     this.coo(:,3) = S * this.coo(:,3);
-            % 
-            %     % apply the S transform to the epochwise parameters
-            %     for i = 1 : n_time
-            %         id_present = ~isnan(this.clock(i,:));
-            %         idx_ref_t = intersect(idx_ref, find(id_present));
-            %         if isempty(idx_ref_t)
-            %             S = nan;
-            %         else
-            %             n_rec_t = sum(id_present);
-            %             S = zeros(n_rec_t);
-            %             S(:,idx_ref) = - 1 / numel(idx_ref_t);
-            %             S = S + eye(n_rec_t);
-            %         end
-            %         % clock
-            %         this.clock(i,:) = (S*this.clock(i,:)')';
-            %         % ztd
-            %         if this.state.flag_tropo
-            %             this.ztd(i,:) = (S*this.ztd(i,:)')';
-            %         end
-            %         % gradients
-            %         if this.state.flag_tropo_gradient
-            %             this.ztd_gn(i,:) = (S*this.ztd_gn(i,:)')';
-            %             this.ztd_ge(i,:) = (S*this.ztd_ge(i,:)')';
-            %         end
-            %     end
-            % end
+
             
             % --- push back the results in the receivers
             % Commented because it is necessary to fix the Network solution before doing this
