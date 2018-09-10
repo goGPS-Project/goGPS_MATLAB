5b0a11954619c81936f08150063899c70455f0c0
clicat <gatti.andrea@gmail.com>
2018-09-10 17:35:52 +0200

    typo fix in clearBranch

--- goGPS/utility/clearBranch.m
+++ goGPS/utility/clearBranch.m
@@ -2,6 +2,6 @@
 warning('off');
 clear all
 clear GO_Settings Logger Main_Settings Settings_Interface Command_Settings Core_UI Constellation_Collector Core Core_Sky ...
-    Receiver_Work_Space Receiver_Output Receiver_Commons GNSS_Station Global_Configuration Command_Interpreter Core_Reference_Frame Meteo_Network Athmosphere Remote_Resource_Manager
+    Receiver_Work_Space Receiver_Output Receiver_Commons GNSS_Station Global_Configuration Command_Interpreter Core_Reference_Frame Meteo_Network Atmosphere Remote_Resource_Manager
 % delete last_settings.ini
 warning('off');
