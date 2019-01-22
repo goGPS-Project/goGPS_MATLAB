goGPS-Project/goGPS_MATLAB

commit 524dcd8256ec82ca9acdf6322ecc9e366c3f5d56

Speed-up reading windows formatted RINEX files

goGPS/obj/Receiver_Work_Space.m
@@ -1646,15 +1646,23 @@ classdef Receiver_Work_Space < Receiver_Commons
                 % open RINEX observation file
                 fid = fopen(file_name,'r');
                 txt = fread(fid,'*char')';
-                txt = txt(txt ~= 13);  % remove carriage return - I hate you Bill!
+                % try to see if carriage return is present in the file (Windows stupid standard)
+                % On Windows file lines ends with char(13) char(10)
+                % instead of just using char(10)
+                if ~isempty(find(txt(1:min(1000,numel(txt))) == 13, 1, 'first'))
+                    has_cr = true;  % The file has carriage return - I hate you Bill!
+                else
+                    has_cr = false;  % The file is UNIX standard
+                end
+                % txt = txt(txt ~= 13);  % remove carriage return - I hate you Bill!
                 fclose(fid);
                 
                 % get new line separators
                 nl = regexp(txt, '\n')';
-                if nl(end) <  numel(txt)
+                if nl(end) <  (numel(txt) - double(has_cr))
                     nl = [nl; numel(txt)];
                 end
-                lim = [[1; nl(1 : end - 1) + 1] (nl - 1)];
+                lim = [[1; nl(1 : end - 1) + 1] (nl - 1 - double(has_cr))];
                 lim = [lim lim(:,2) - lim(:,1)];
                 while lim(end,3) < 3
                     lim(end,:) = [];

commit 80ca34661955f0638fda5abd89c0fe8967584a58

Fix a bug  introduced by commit 524dcd8256ec82ca9acdf6322ecc9e366c3f5d56
On RINEX2, due to carriage returns no more removed in the raw txt variable , the reading of satellite names was wrong when more than 12 satellites were in view

goGPS/obj/Receiver_Work_Space.m
@@ -1679,7 +1679,7 @@ classdef Receiver_Work_Space < Receiver_Commons
                 
                 if (this.rin_type < 3)
                     % considering rinex 2
-                    this.parseRin2Data(txt, lim, eoh, t_start, t_stop, rate);
+                    this.parseRin2Data(txt, has_cr, lim, eoh, t_start, t_stop, rate);
                 else
                     % considering rinex 3
                     this.parseRin3Data(txt, lim, eoh, t_start, t_stop, rate);
@@ -2113,7 +2113,7 @@ classdef Receiver_Work_Space < Receiver_Commons
             
         end
         
-        function parseRin2Data(this, txt, lim, eoh, t_start, t_stop, rate)
+        function parseRin2Data(this, txt, has_cr, lim, eoh, t_start, t_stop, rate)
             % Parse the data part of a RINEX 2 file -  the header must already be parsed
             % SYNTAX this.parseRin2Data(txt, lim, eoh, <t_start>, <t_stop>)
             % remove comment line from lim
@@ -2262,7 +2262,9 @@ classdef Receiver_Work_Space < Receiver_Commons
                 for e = 1 : n_epo % for each epoch
                     n_sat = this.n_spe(e);
                     % get the list of satellites in view
-                    sat = serialize(txt(lim(t_line(e),1) + repmat((0 : ceil(this.n_spe(e) / 12) - 1)' * 69, 1, 36) + repmat(32:67, ceil(this.n_spe(e) / 12), 1))')';
+                    nlps = ceil(this.n_spe(e) / 12); % number of lines used to store satellite names
+                    id_sat = serialize((lim(t_line(e),1) + repmat((0 : nlps - 1)' * (69 + has_cr), 1, 36) + repmat(32:67, nlps, 1))');                    
+                    sat = txt(id_sat(1 : n_sat * 3));
                     sat = sat(1:n_sat * 3);
                     sat = reshape(sat, 3, n_sat)';
                     sat(sat(:,1) == 32) = this.rinex_ss;