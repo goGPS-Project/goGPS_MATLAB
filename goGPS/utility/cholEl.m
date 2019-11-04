63e8c8bc9f61b46518fc56c1fcbce6e8fc47a9f0
giuliot <giulio.tagliaferro@g-red.eu>
2019-11-04 19:35:22 +0100

    chol rem

---
+++ goGPS/utility/cholEl.m
@@ -0,0 +1,34 @@
+function [R,idx_rm] = cholEl(A,tol)
+% perform cholesky eliminating rank deficient columns 
+% A must be sparse
+%
+% SYNTAX:
+% [R,idx_rm] = cholEl(A,tol)
+R = zeros(size(A));
+idx_rm = [];
+g = 1;
+idx_a = 0;
+idx_r = 1;
+while g ~=0 && idx_r < (size(A,1) - length(idx_rm))
+    idx_a = idx_a + g;
+    
+    [Rt,g] = chol(A(idx_a:end,idx_a:end),'lower');
+    if g == 0
+        diag_el = diag(Rt);
+        idx_rm_r = diag_el < tol;
+        idx_rm = [idx_rm; idx_a-1+find(idx_rm_r)];
+        
+        R(idx_r:(end-length(idx_rm)),idx_r:(end-length(idx_rm))) = Rt;
+    else
+        diag_el = diag(Rt(1:(g-1),:));
+        idx_rm_r = diag_el < tol;
+        idx_rm = [idx_rm; idx_a-1+find(idx_rm_r); idx_a-1+g];
+        R(idx_r+g-1:end-1,:) = R(idx_r+g:end,:);
+        R(idx_r:(end - length(idx_rm)),idx_r+(0:(g-1-1-sum(idx_rm_r)))) = Rt(1:size(Rt,1) ~= g,:);
+    end
+    idx_r = idx_r +g  -sum(idx_rm_r) -1;
+end
+R(:,end-length(idx_rm)+1:end) = [];
+R(end-length(idx_rm)+1:end,:) = [];
+end
+
