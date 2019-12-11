0217ee67e6a5f96bfab8b49d4defcfd0bb966fbc
Andrea <gatti.andrea@gmail.com>
2019-12-11 17:12:55 +0100

    Force GUI_Msgwhen using GUI
    As default Logger use command line as message status

--- goGPS/goEditSettings.m
+++ goGPS/goEditSettings.m
@@ -1,5 +1,7 @@
 % Shortcut to open the main settings editor of goGPS
 addPathGoGPS
+log = Core.getLogger();
+log.setOutMode(0,[],1);
 ui = Core_UI.getInstance();
 flag_wait = false;
 ui.openGUI(flag_wait);
--- goGPS/goHelpCommands.m
+++ goGPS/goHelpCommands.m
@@ -1,3 +1,5 @@
 % Shortcut to GUI_Command_Help
 addPathGoGPS
+log = Core.getLogger();
+log.setOutMode(0,[],1);
 GUI_Command_Help
