4cf3760736846fe92d44e7a67f86665e06721d97
Andrea <gatti.andrea@gmail.com>
2020-01-16 17:39:09 +0100

    Put labels on top if axis in polarScatter

--- goGPS/plot/polarScatter.m
+++ goGPS/plot/polarScatter.m
@@ -67,9 +67,8 @@ function h = polarScatter(az, decl, point_size, color, flag, plot_bg)
         for d = decl_s
             x = cos(az_l).*d;
             y = sin(az_l).*d;
-            text(cos(80/180*pi)*d,sin(80/180*pi)*d,sprintf('%d',round(d*90)),'HorizontalAlignment','center', 'FontWeight', 'bold');
             plot(x,y,'color',[0.6 0.6 0.6]);
-            
+            text(cos(80/180*pi)*d,sin(80/180*pi)*d,sprintf('%d',round(d*90)),'HorizontalAlignment','center', 'FontWeight', 'bold');            
         end
         %plot meridian
         az_step = 30/180 *pi;
@@ -78,10 +77,10 @@ function h = polarScatter(az, decl, point_size, color, flag, plot_bg)
         for a = az_s
             x = cos(a).*decl_l;
             y = sin(a).*decl_l;
+            plot(x,y,'color',[0.6 0.6 0.6]);
             if abs(a-2*pi) > 0.0001
                 text(cos(a)*1.1,sin(a)*1.1,sprintf('%d', mod(round((2*pi - a + pi/2) / pi * 180), 360)), 'HorizontalAlignment','center', 'FontWeight', 'bold');
             end
-            plot(x,y,'color',[0.6 0.6 0.6]);            
         end
         axis equal
         % xlim([-2 2])
