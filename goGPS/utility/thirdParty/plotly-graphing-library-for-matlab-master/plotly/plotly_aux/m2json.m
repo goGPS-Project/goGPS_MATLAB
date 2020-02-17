23a69f3ec9c7c0044f37a2dc693513733eedba65
Andrea <gatti.andrea@gmail.com>
2020-02-17 15:58:24 +0100

    Manage multi-line strings in m2json -> plotly API

--- goGPS/utility/thirdParty/plotly-graphing-library-for-matlab-master/plotly/plotly_aux/m2json.m
+++ goGPS/utility/thirdParty/plotly-graphing-library-for-matlab-master/plotly/plotly_aux/m2json.m
@@ -25,8 +25,12 @@ function valstr = m2json(val)
         valstr = strrep(valstr, 'Inf', 'null');
         valstr = strrep(valstr, 'NaN', 'null');
     elseif ischar(val)
-         val = checkescape(val); %add escape characters
-         valstr = ['"' val '"'];
+        if size(val, 1) > 1
+            valstr = cell2json(cellstr(val));
+        else
+            val = checkescape(val); %add escape characters
+            valstr = ['"' val '"'];
+        end
     elseif islogical(val)
         if val
             valstr = 'true';
