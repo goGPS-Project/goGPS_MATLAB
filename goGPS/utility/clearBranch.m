ffd28ec84fafe1e90f0761d4ca6c967007a1ec9a
Andrea <gatti.andrea@gmail.com>
2020-07-16 16:03:45 +0000

    Add Command_Interpreter to clearBranch

--- goGPS/utility/clearBranch.m
+++ goGPS/utility/clearBranch.m
@@ -2,6 +2,6 @@
 warning('off');
 GUI_Edit_Settings.closeGUI
 clear all
-clear Logger Prj_Settings Go_Settings Settings_Interface Command_Settings Core_UI GUI_Edit_Settings Core Remote_Resource_Manager Parallel_Manager Go_Slave Go_Wait_Bar Zernike
+clear Logger Prj_Settings Go_Settings Settings_Interface Command_Interpreter Command_Settings Core_UI GUI_Edit_Settings Core Remote_Resource_Manager Parallel_Manager Go_Slave Go_Wait_Bar Zernike
 % delete last_settings.ini
 warning('off');
